5ba6467e7920a8aa162b6e3fb3c4c8aebc8c2ded
joshuathawke <joshuathawke@gmail.com>
2023-04-03 12:39:32 -0400

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-
+# greetProgrammer function
 def greet_programmer():
-    pass
-
+    print("Hello, programmer!")
+    
+# greet function
 def greet(name):
-    pass
-
+    print(f"Hello, {name}!")
+    
+# greetWithDefault function
 def greet_with_default(name="programmer"):
-    pass
-
+    print(f"Hello, {name}!")
+    
+# add function
 def add(num1, num2):
-    pass
-
+    return num1 + num2
+    
+# halve function
 def halve(number):
-    pass
+    if type(number) != int and type(number) != float:
+        return None
+    return number / 2
